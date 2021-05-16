c78a4a578ad5ac2af5093e50b9352e93f137507d
Wendy <wendytsao84@gmail.com>
2021-05-16 17:06:39 +0800

    extract Graphic component

---
+++ src/components/Graphic.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="portfolio-item col-lg-4">
+    <img :src="require(`@/assets/pic/${content.imgUrl}`)" />
+    <div class="text">
+      <h3>{{ content.title }}</h3>
+      <h3>{{ content.subTitle }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: Object,
+    },
+  }
+};
+</script>
+
+<style>
+</style>
