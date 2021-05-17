2b8b0598ccece16e2b93d009c8f9b2cbbc2cc3b2
Wendy <wendytsao84@gmail.com>
2021-05-17 18:04:28 +0800

    extract Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container-fluid footer">
+    <slot></slot>
+    <div class="portfolioLink">
+      <a v-for="link in portfolioLink" :key="link.iconLink"
+        :href="link.iconLink">
+        <i :class="link.iconClass"></i>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const portfolioLink = [
+      { iconLink: "#", iconClass: "fab fa-github-alt" },
+      { iconLink: "#", iconClass: "fab fa-codepen" },
+      { iconLink: "#", iconClass: "fas fa-envelope" },
+    ];
+
+    return { portfolioLink };
+  },
+};
+</script>
+
+<style>
+</style>
