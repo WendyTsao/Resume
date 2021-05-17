e016c13bf3a53b70c72ba6b7fa62e107021a2790
Wendy <wendytsao84@gmail.com>
2021-05-17 17:03:53 +0800

    extract CardList component

---
+++ src/components/CardList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body text">
+        <h4>{{ title }}</h4>
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
