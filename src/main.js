005de4e1554b17579a5fe75dd0fd9fe9e0a2283f
Wendy <wendytsao84@gmail.com>
2021-06-09 16:43:07 +0800

    add AOS animation

--- src/main.js
+++ src/main.js
@@ -6,4 +6,9 @@ createApp(App).use(router).mount('#app')
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import './assets/css/style.css';
+
+AOS.init();
